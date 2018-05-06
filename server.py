cff980203448ccc23e0e2678ab8b5e9d118f9668
Tomos Evans <TomosAMEvans@gmail.com>
2018-05-06 12:39:12 +0100

    merged all into one

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO,  emit
+from flask_restful import Api, Resource
+# import RPi.GPIO as GPIO
+import time
 
 
 
@@ -7,6 +10,8 @@ from flask_socketio import SocketIO,  emit
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+api      = Api(app)
+
 
 @app.route('/', methods=['GET'])
 def timer():
@@ -18,3 +23,52 @@ def timer_update():
     new_time_seconds=request.get_json()['time']
     socketio.emit('timer_update', new_time_seconds)
     return 'ok', 200
+
+
+alarm_pin=21
+
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(alarm_pin, GPIO.OUT)
+
+alarm_is_on = False
+
+
+current_time = lambda: int(round(time.time() *1000))
+
+# Grant my..
+last_request = 0
+# and let me hold you..
+
+class RaiseAlarmResource(Resource):
+    def post(self):
+        global alarm_is_on
+        global last_request
+        this_request = current_time()
+        if alarm_is_on or (this_request-last_request < 5000):
+            pass
+        else:
+            last_request = this_request
+
+            # GPIO.output(alarm_pin, GPIO.LOW)
+            alarm_is_on=True
+        return 200
+
+class LowerAlarmResource(Resource):
+    def post(self):
+        global alarm_is_on
+        global last_request
+        this_request = current_time()
+        if not alarm_is_on or (this_request-last_request < 5000):
+            pass
+        else:
+            last_request = this_request
+            # GPIO.output(alarm_pin, GPIO.HIGH)
+            alarm_is_on=False
+        return 200
+
+# GPIO.output(alarm_pin, GPIO.HIGH)
+
+api.add_resource(RaiseAlarmResource, '/alarm/raise', endpoint='raise')
+api.add_resource(LowerAlarmResource, '/alarm/lower', endpoint='lower')
+
+# app.run(host='0.0.0.0')
